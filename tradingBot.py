080ae11bac2e6857c31beeb1a6ce4192ca37b089
n33lp <canada0706@gmail.com>
2024-02-11 12:40:36 -0700

    Add MLTrader class and backtesting strategy

--- tradingBot.py
+++ tradingBot.py
@@ -14,3 +14,26 @@ ALPACA_CREDENTIALS = {
     "PAPER_TRADE" : True
 }
 
+start_date = datetime(2023,12,15)
+end_date = datetime(2023,12,31)
+
+class MLTrader(Strategy):
+    def initialize(self, symbol:str="SPY"):
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+
+
+    def on_trading_iteration(self):
+        if self.last_trade == None:
+            order = self.create_order(
+                self.symbol, 10, "buy", type="market"
+            )
+            self.submit_order(order)
+            self.last_trade="buy"
+
+
+broker = Alpaca(ALPACA_CREDENTIALS)
+strategy = MLTrader(name="mlstrat", broker = broker, parameters={"symbol": "SPY"})
+
+strategy.backtest( YahooDataBacktesting, start_date, end_date, parameters={"symbol": "SPY"})
